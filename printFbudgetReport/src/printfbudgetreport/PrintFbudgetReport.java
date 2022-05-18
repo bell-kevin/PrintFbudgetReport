6e056f2db6bcf3247d59473f30f4b09ee4bccdd7
Kevin Bell <bellKevin@pm.me>
2022-05-18 02:03:24 +0000

    Add files via upload

---
+++ printFbudgetReport/src/printfbudgetreport/PrintFbudgetReport.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package printfbudgetreport;
+
+/**
+ *
+ * @author 4800590195
+ */
+public class PrintFbudgetReport {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        final String HEADING_FMT_STR = "%-25s%13s%13s%15s\n";
+        final String DATA_FMT_STR = "%-25s%,13.2f%,13.2f%(,15.2f\n";
+        double actual1 = 1149.999;
+        double budget1 = 1400;
+        double actual2 = 2100.111;
+        double budget2 = 2000;
+        double remaining1, remaining2;
+
+        System.out.printf(HEADING_FMT_STR, "Account", "Actual", "Budget", "Remaining");
+        System.out.printf(HEADING_FMT_STR, "-------", "------", "------", "----------");
+
+        remaining1 = budget1 - actual1;
+        System.out.printf(DATA_FMT_STR, "Office Supplies", actual1, budget1, remaining1);
+        remaining2 = budget2 - actual2;
+        System.out.printf(DATA_FMT_STR, "Photocopying", actual2, budget2, remaining2);
+        System.out.printf("\nTotal remaining: $%(,.2f\n", remaining1 + remaining2);
+    }
+}
